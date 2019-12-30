12deb83145bb5a1a44041a7388af8595bcd55b8f
nvhabib91 <nvhabib91@gmail.com>
2019-12-29 16:07:25 -0800

    Update main.py

---
+++ PyBank/main.py
@@ -0,0 +1,15 @@
+#PyBank
+import os, csv
+
+pybank = os.path.join("..", "Resources", "budget_data.csv")
+
+#The total number of months included in the dataset
+
+def pybank_analysis(pybank):
+    profits_lossess = {
+    "months": months[0],
+    "p_l": int(proloss[1])
+    }
+
+print(pybank_analysis(months))
+    
